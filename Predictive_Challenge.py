8a86880835b7da1df27a5e7f5a22474333c24b0e
Manuela Andrea Beyer <67337290+Miuuu13@users.noreply.github.com>
2023-12-14 08:45:00 +0100

    Delete Predictive_Challenge.py

--- Predictive_Challenge.py
+++
@@ -1,8 +0,0 @@
-#EAI Winter school 2023
-#Predictive Challenge
-#The goal is to predict PM2.5_target, the one-hour ahead PM2.5 concentration, on the test set.
-
-
-# https://jldc.ch/uploads/train.pkl
-
-# https://jldc.ch/uploads/test.pkl
